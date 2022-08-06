0b9f52b8333c4cc0cba62e491c030030d8c1bbc8
sirvvy <henrywilliams186@gmail.com>
2022-08-06 13:32:58 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'z-red': '#e7332b',
+        'z-charcoal': '#212529',
+        'z-gray': '#514949',
+        'z-pink': '#fffafa',
+        'z-blue': '#0000ee',
+        'z-ash': '#787878',
+        'z-newpink': '#fffdf5',
+        'z-black': '#161617',
+        'z-green': '#43b97f'
+      },
+    },
+  },
+  plugins: [],
+}
